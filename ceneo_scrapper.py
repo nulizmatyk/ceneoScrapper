ac07c5ce4ebf9caf40c945d4234808fab9c3718f
unknown <m.otreba@polsource.com>
2018-12-06 16:04:11 +0100

    add gui

--- ceneo_scrapper.py
+++ ceneo_scrapper.py
@@ -1,30 +1,46 @@
 import urllib.request
+from tkinter import *
 from bs4 import BeautifulSoup
 import json
 from time import sleep 
 
-
-def extract_opinions
-
-url = "https://www.ceneo.pl/35379099"
-
 #Adding a User-Agent String in the request to prevent getting blocked while scraping 
+
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
-request = urllib.request.Request(url,headers={'User-Agent': user_agent})
 
-html = urllib.request.urlopen(request).read()
-soup = BeautifulSoup(html,'html.parser')
+def go():
+    text.delete(1.0, END)
+    request = urllib.request.Request(entry.get(),headers={'User-Agent': user_agent})
 
-#First lets get the HTML of the table called site Table where all the links are displayed
-main_table = soup.find("div",attrs={'class':'page-tab-content click no-padding wrapper', })
+    html = urllib.request.urlopen(request).read()
+    soup = BeautifulSoup(html,'html.parser')
 
-offers = main_table.find_all("tr",attrs={'class': ['product-offer js_product-offer', 'promoted']})
-extracted_data = []
+    #First lets get the HTML of the table called site Table where all the links are displayed
+    main_table = soup.find("div",attrs={'class':'page-tab-content click no-padding wrapper', })
 
-for a_tag in offers:
-    print(a_tag['data-shopurl'])
+    offers = main_table.find_all("tr",attrs={'class': ['product-offer js_product-offer', 'promoted']})
+    extracted_data = []
+
+    for a_tag in offers:
+        # print(a_tag['data-shopurl'])
+       text.insert(1.0, a_tag['data-shopurl']) 
+
+
+    print(len(offers))
     
+    
+browser_window = Tk()
+browser_window.title('jakis tytul')
+label = Label(browser_window, text ='Enter Url: ')
+entry = Entry(browser_window)
+entry.insert(END, "")
+button = Button(browser_window, text='go', command= go)
+text = Text(browser_window)
+label.pack(side = TOP)
+entry.pack(side = TOP)
+button.pack(side = TOP)
+text.pack(side = TOP)
+
+browser_window.mainloop()
 
 
-print(len(offers))
-# print(offers)
