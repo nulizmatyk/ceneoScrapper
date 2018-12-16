caa9ccf4e4cb8881089f6c439a125e5c080075ff
unknown <m.otreba@polsource.com>
2018-12-16 21:52:40 +0100

    scrapper is finished

--- ceneo_scrapper.py
+++ ceneo_scrapper.py
@@ -1,3 +1,4 @@
+import json
 import urllib.request
 from tkinter import *
 from bs4 import BeautifulSoup
@@ -9,43 +10,117 @@ import MySQLdb
 # Open database connection
 HOST = "localhost"
 USERNAME = "root"
-PASSWORD = "Mateusz"
-DATABASE = "scraping_sample"
-db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE)
-# prepare a cursor object using cursor() method
-cursor = db.cursor()
-# Prepare SQL query to INSERT a record into the database.
-sql = "INSERT INTO classes(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', '{}', {})".format(name_of_class, type_of_course, lecturer, number, short_text, choice_term, hours_per_week_in_term, expected_num_of_participants, maximum_participants, assignment, lecture_id, credit_points, hyperlink, language, 'NOW()')
-try:
- # Execute the SQL command
- cursor.execute(sql)
- # Commit your changes in the database
- db.commit()
-except:
- # Rollback in case there is any error
- db.rollback()
- #get the just inserted class id
-sql = "SELECT LAST_INSERT_ID()"
-try:
- # Execute the SQL command
- cursor.execute(sql)
- # Get the result
- result = cursor.fetchone()
- # Set the class id to the just inserted class
- class_id = result[0]
-except:
- # Rollback in case there is any error
- db.rollback()
- # disconnect from server
- db.close()
- # on error set the class_id to -1
- class_id = -1
-#  ##############################################################################################
-#Adding a User-Agent String in the request to prevent getting blocked while scraping 
-
+PASSWORD = ""
+DATABASE = "testowa"
+db = MySQLdb.connect(HOST, USERNAME, PASSWORD, DATABASE, use_unicode=True, charset="utf8")
+url = ""
+idOfProduct = ""
+nameOfProduct = ""
+listOfOpinions = []
+extractedData = {}
+extractedData['opinions'] = []
 user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 numberOfPage = 1
-extracted_data = []
+data = 1
+reviews = []
+class Rewiew:
+  def __init__(self, name, recomendOrNot, comment, score, nameOfProduct, idOfProduct ):
+    self.name = name
+    self.recomendOrNot = recomendOrNot
+    self.comment = comment
+    self.score = score
+    self.nameOfProduct = nameOfProduct
+    self.idOfProduct = idOfProduct
+
+def clearDataBase():
+    sql = "TRUNCATE TABLE rewiewers"
+    cursor = db.cursor()
+    cursor.execute(sql)
+    # Commit your changes in the database
+    db.commit()
+    infoText.delete('1.0',END)
+    infoText.insert(1.0, "Z bazy danych usunięto wszystkie rekordy")
+
+def processETL():
+    extractData()
+    transform()
+    saveDatatoDataBase()
+    
+    
+def saveDatatoDataBase():
+    global numberOfPage
+    global idOfProduct 
+    global nameOfProduct 
+    sql = "DELETE FROM rewiewers WHERE id_of_product_on_website = %s"
+    id = (idOfProduct, )
+    cursor = db.cursor()
+    cursor.execute(sql, id)
+    db.commit()
+    for opinion in listOfOpinions:
+        cursor = db.cursor()
+        sql = "INSERT INTO rewiewers(id_of_product_on_website, name_of_product, name_of_reviewer, recomended_or_not, comment, score) VALUES ('{}','{}','{}', '{}', '{}', '{}')".format(idOfProduct, nameOfProduct, opinion.name , opinion.recomendOrNot , opinion.comment, opinion.score)
+            
+        # Prepare SQL query to INSERT a record into the database.
+        try:
+            # Execute the SQL command
+            cursor.execute(sql)
+            # Commit your changes in the database
+            db.commit()
+        except:
+            print("catch")
+            # Rollback in case there is any error
+            db.rollback()
+            # #get the just inserted class id
+    komunikat = 'Do bazy danych zapisano ' + str(len(listOfOpinions))  + ' rekordy/ów'
+    infoText.delete('1.0',END)
+    infoText.insert(1.0, komunikat)
+    buttonETL['state'] = 'normal'
+    buttonExtract['state'] = 'normal'
+    buttonTransform['state'] = 'disable'
+    buttonSave['state'] = 'disable'
+    buttonExport['state'] = 'disable'
+    reviews.clear()
+    listOfOpinions.clear()
+    numberOfPage = 1
+    nameOfProduct = ""
+    idOfProduct = ""
+    
+      
+def extractData():
+    global numberOfPage
+    global idOfProduct 
+    global nameOfProduct 
+    text.delete('1.0',END)
+    url = 'https://www.ceneo.pl/' + entry.get() + '/opinie-' + str(numberOfPage)
+    idOfProduct = entry.get()
+    numberOfPage += 1
+    rewiewsForOneProduct = []
+    request = urllib.request.Request(url, headers={'User-Agent': user_agent})
+
+    html = urllib.request.urlopen(request).read()
+    soup = BeautifulSoup(html,'html.parser')
+
+    #First lets get the HTML of the table called site Table where all the links are displayed
+    main_table = soup.find("div",attrs={'class':'screening-wrapper' })
+    nameOfProduct = main_table.find("h2",attrs={'class': 'section-title with-context header-curl'}).text.replace('- Opinie', '')
+    # all reviews on site
+    reviews.extend(main_table.find_all("li",attrs={'class': 'review-box js_product-review'}))
+
+    if checkIFMoreExist(main_table) == True :
+        extractData()
+    else:
+        # reviews.extend(rewiewsForOneProduct)
+        komunikat = 'Ze strony sciagnieto ' + str(len(reviews))  + ' opinii'
+        infoText.delete('1.0',END)
+        infoText.insert(1.0, komunikat)
+        buttonTransform['state'] = 'normal'     
+        buttonExtract['state'] = 'disable'
+        buttonETL['state'] = 'disable'
+
+    #    saveDatatoDataBase()
+#Adding a User-Agent String in the request to prevent getting blocked while scraping 
+# user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
 # function to check if there is more opinions
 def checkIFMoreExist(page):
     more = []
@@ -55,51 +130,49 @@ def checkIFMoreExist(page):
     else :
         return False
 
-# function which is run by button and doing all process of data extraction
-def go():
-    global numberOfPage
-    
-    # do loop with every page with reviews
-    url = 'https://www.ceneo.pl/' + entry.get() + '/opinie-' + str(numberOfPage)
-    numberOfPage += 1
-
-    request = urllib.request.Request(url, headers={'User-Agent': user_agent})
-
-    html = urllib.request.urlopen(request).read()
-    soup = BeautifulSoup(html,'html.parser')
-
-    #First lets get the HTML of the table called site Table where all the links are displayed
-    main_table = soup.find("div",attrs={'class':'screening-wrapper' })
-        
-    # all reviews on site
-    reviews = main_table.find_all("li",attrs={'class': 'review-box js_product-review'})
-        
-    # loop which takes every review from pool
+# function which transform data
+def transform():
     for a_tag in reviews:
-            
-        # score 
-        # extracted_data.append(a_tag.find("span",attrs={'class':'review-score-count'}).text)
-        score = a_tag.find("span",attrs={'class':'review-score-count'}).text
-        nameOfReviewer = a_tag.find("div",attrs={'class':'reviewer-name-line'}).text
+        score = a_tag.find("span",attrs={'class':'review-score-count'}).text.replace("'","`") 
+        nameOfReviewer = a_tag.find("div",attrs={'class':'reviewer-name-line'}).text.replace("'","`") 
         recomendOrNot = a_tag.find("em",attrs={'class': ['product-recommended', 'product-not-recommended']})
 
         # check if field with recomendation isn't empty beacuse it can be left blank and then, there is no em with this class
         if recomendOrNot != None and recomendOrNot != 'null':
-            recomendOrNot = recomendOrNot.text
+            recomendOrNot = recomendOrNot.text.replace("'","`") 
         else:
             recomendOrNot = ''
-        comment = a_tag.find("p",attrs={'class':'product-review-body'}).text
-        text.insert(1.0, nameOfReviewer + ' - ' + recomendOrNot + ' - ' + comment + ' - ' + score )
-        extracted_data.append(nameOfReviewer)
+        # there is a need to replace character ' with blank space because of sql
+        comment = a_tag.find("p",attrs={'class':'product-review-body'}).text.replace("'","`") 
+        text.insert(1.0,nameOfProduct + ' - ' + nameOfReviewer + ' - ' + recomendOrNot + ' - ' + comment + ' - ' + score )
         text.insert(1.0, '')
+        rewiew = Rewiew(nameOfReviewer, recomendOrNot, comment, score, nameOfProduct, idOfProduct)
+        listOfOpinions.append(rewiew)
+        extractedData['opinions'].append({  
+        'nameOfProduct': nameOfProduct,
+        'idOfProduct': idOfProduct,
+        'nameOfReviewer': nameOfReviewer,
+        'recomendOrNot': recomendOrNot,
+        'comment': comment,
+        'score': score
+        })
+    komunikat = 'Przetransformowano ' + str(len(listOfOpinions)) + ' opinii'
+    infoText.delete('1.0',END)
+    infoText.insert(1.0, komunikat)    
+    buttonSave['state'] = 'normal'
+    buttonExport['state'] = 'normal'
+    buttonTransform['state'] = 'disable'
 
-    # jest tests
-    print(len(extracted_data))
-    print(url)
-    print(checkIFMoreExist(main_table))
-    # text.insert(1.0, extracted_data)
-    if checkIFMoreExist(main_table) == True :
-        go()
+   
+
+    # jest test
+    # saveDatatoDataBase()
+
+def exportDatatoJson():
+    with open('ceneo.json', 'w') as outfile:
+        json.dump(extractedData, outfile, indent=4)
+
+    
 
 
 
@@ -110,15 +183,28 @@ browser_window.geometry("1000x1000")
 browser_window.title('CENEO SCRAPPER')
 label = Label(browser_window, text ='Enter product Id: ')
 entry = Entry(browser_window)
-entry.insert(END, "35379099")
-button = Button(browser_window, text='go', command= go)
+entry.insert(END, "")
+buttonExtract = Button(browser_window, text='Extract Data', command= extractData)
+buttonTransform = Button(browser_window, text='Transform Data', command= transform, state=DISABLED)
+buttonSave = Button(browser_window, text='Save Data', command= saveDatatoDataBase, state=DISABLED)
+buttonETL = Button(browser_window, text='ETL Process', command= processETL)
+buttonClear = Button(browser_window, text='Clear DataBase', command= clearDataBase)
+buttonExport = Button(browser_window, text='ExportData', command= exportDatatoJson, state=DISABLED)
 text = Text(browser_window, height= 100, width = 500)
+infoText =  Text(browser_window, height= 2, width = 30)
 
 # insert parts of gui to in window
 label.pack(side = TOP)
 entry.pack(side = TOP)
-button.pack(side = TOP)
+buttonExtract.pack(side = TOP, fill=BOTH, expand=1)
+buttonTransform.pack(side = TOP, fill=BOTH, expand=1)
+buttonSave.pack(side = TOP, fill=BOTH, expand=1)
+buttonETL.pack(side = TOP, fill=BOTH, expand=1)
+buttonClear.pack(side = TOP, fill=BOTH, expand=1)
+buttonExport.pack(side = TOP, fill=BOTH, expand=1)
+infoText.pack(side = TOP)
 text.pack(side = TOP)
+
 
 # opens window
 browser_window.mainloop()
